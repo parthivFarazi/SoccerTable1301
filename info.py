601049063a5226a66e4e3b15a471588693123a94
parthivFarazi <parthivfarazi@icloud.com>
2025-03-24 17:00:39 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/profile1.webp" #Change this
+about_me = "I'm Brent Key. The key to Georgia Tech Yellow Jacket College Football team's success" 
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/brent-key-5270b2a"
+my_github_url = "https://github.com/your-github-username"
+my_email_address = "bkey3@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Sports Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '5.0'
+}
+course_data = {
+    "code":["GT 1002", "WF 1004", "FL 1006 ", "GT 1008"], 
+    "names":["SC State", "Wake Forest", "U Miami", "UNC"], 
+    "semester_taken":["1st", "2nd", "3rd", "4th"],
+    "skills":["Start of something great", "Cant catch us on the road", "CHRISTIAN LEAARRYYYY", "Haynes King is my son"],
+    }
+experience_data = {
+    "What is Tar Heel?": (["- Worked on turning Haynes King into a monster",
+                                                                          "- Gave UNC their L", "- BEST HOME WIN OF THE SEASON"],"images/tarhill.jpg"),#change this 
+    "Rank teams killer":(["- Pulled an absolute upset against U Miami",
+                                                           "- Gave U Miami their 1st L"],"images/LEAARRYYY.jpeg"),#change this 
+    "Bowl Bound":(["- Led GT to their first bowl game in 5 years"],"images/kingkey.jpeg")#change this
+
+}
+
+projects_data = {
+    "The Standings": "Created an app for all the standings of soccer leagues (La Liga, EPL, Serie A and Ligue 1) since 2011-12 season",
+}
+
+programming_data = {
+    "Passion": 100,
+    "Attack": 70,
+    "Defense": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Passion": "❤️‍🔥",
+    "Attack": "🅰️",
+    "Defence": "🛡️",
+}
+spoken_icons = {"Tech Man": "👨🏻‍🔧",
+    "English": "🇬🇧",
+    "Winning":"🐝"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "Tech Man": "By Birth",
+    "Winning": "In DNA",
+    "English": "Fluent",
+}
+leadership_data = {
+    "Leading GT as the head coach": (["- Led to GT's best record in many years"],"images/keyGOAT.png"),
+
+}
+activity_data={
+    "Helluva Coach": ["- Turned Haynes King into his son", 
+            "- HE GOT THE KEY!!! 🔑"]
+}
